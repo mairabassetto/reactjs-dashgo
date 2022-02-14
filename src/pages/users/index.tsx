00eaa218c6c9a1bc82a089d96a7737701368ac8a
mairabassetto <mairabassetto@gmail.com>
2022-02-14 20:13:50 -0300

    Configuring React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,23 +1,25 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
-import { useEffect } from "react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
 import { RiAddLine } from "react-icons/ri";
+import { useQuery } from 'react-query';
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery('users', async() => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+    
+    return data;
+  })
+  
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   })
 
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then(response => response.json())
-      .then(data => console.log(data))
-  }, [])
-  
   return(
     <Box>
       <Header />
@@ -43,60 +45,72 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th paddingX={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink"/>
-                </Th>
-                <Th>Usuário</Th>
-                { isWideVersion && <Th>Data de cadastro</Th> }
-                <Th width={8}></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td paddingX={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold"> Maíra Bassetto </Text>
-                    <Text fontSize="sm" color="gray.300"> mairabassetto@gmail.com </Text>
-                  </Box>
-                </Td>
-              </Tr>
-                
-              <Tr>
-                <Td paddingX={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold"> Maíra Bassetto </Text>
-                    <Text fontSize="sm" color="gray.300"> mairabassetto@gmail.com </Text>
-                  </Box>
-                </Td>
-                { isWideVersion && <Td>26 de Janeiro, 2022</Td> }
-              </Tr>
+          { isLoading ? (
+            <Flex justify='center'>
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify='center'>
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th paddingX={["4", "4", "6"]} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink"/>
+                    </Th>
+                    <Th>Usuário</Th>
+                    { isWideVersion && <Th>Data de cadastro</Th> }
+                    <Th width={8}></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td paddingX={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold"> Maíra Bassetto </Text>
+                        <Text fontSize="sm" color="gray.300"> mairabassetto@gmail.com </Text>
+                      </Box>
+                    </Td>
+                  </Tr>
+                    
+                  <Tr>
+                    <Td paddingX={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold"> Maíra Bassetto </Text>
+                        <Text fontSize="sm" color="gray.300"> mairabassetto@gmail.com </Text>
+                      </Box>
+                    </Td>
+                    { isWideVersion && <Td>26 de Janeiro, 2022</Td> }
+                  </Tr>
 
-              <Tr>
-                <Td paddingX={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold"> Maíra Bassetto </Text>
-                    <Text fontSize="sm" color="gray.300"> mairabassetto@gmail.com </Text>
-                  </Box>
-                </Td>
-                { isWideVersion && <Td>26 de Janeiro, 2022</Td> }
-              </Tr>
+                  <Tr>
+                    <Td paddingX={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold"> Maíra Bassetto </Text>
+                        <Text fontSize="sm" color="gray.300"> mairabassetto@gmail.com </Text>
+                      </Box>
+                    </Td>
+                    { isWideVersion && <Td>26 de Janeiro, 2022</Td> }
+                  </Tr>
 
-            </Tbody>
-          </Table>
+                </Tbody>
+              </Table>
 
-          <Pagination />
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
