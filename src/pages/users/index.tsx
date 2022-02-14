069db1c725548bbbb6e5ec1439bc62c47dc3984c
mairabassetto <mairabassetto@gmail.com>
2022-02-14 20:39:14 -0300

    Users list

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,7 +12,19 @@ export default function UserList() {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json()
     
-    return data;
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    })
+    return users;
   })
   
   const isWideVersion = useBreakpointValue({
@@ -67,44 +79,22 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td paddingX={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold"> Maíra Bassetto </Text>
-                        <Text fontSize="sm" color="gray.300"> mairabassetto@gmail.com </Text>
-                      </Box>
-                    </Td>
-                  </Tr>
-                    
-                  <Tr>
-                    <Td paddingX={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold"> Maíra Bassetto </Text>
-                        <Text fontSize="sm" color="gray.300"> mairabassetto@gmail.com </Text>
-                      </Box>
-                    </Td>
-                    { isWideVersion && <Td>26 de Janeiro, 2022</Td> }
-                  </Tr>
-
-                  <Tr>
-                    <Td paddingX={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold"> Maíra Bassetto </Text>
-                        <Text fontSize="sm" color="gray.300"> mairabassetto@gmail.com </Text>
-                      </Box>
-                    </Td>
-                    { isWideVersion && <Td>26 de Janeiro, 2022</Td> }
-                  </Tr>
-
+                  {data.map(user => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td paddingX={["4", "4", "6"]}>
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold"> {user.name} </Text>
+                            <Text fontSize="sm" color="gray.300"> {user.email}</Text>
+                          </Box>
+                        </Td>
+                        { isWideVersion && <Td>{user.createdAt}</Td> }
+                      </Tr>
+                    )
+                  })}
                 </Tbody>
               </Table>
 
