f83ccc1cfb8e556b019e39b0360a9d72cb692286
mairabassetto <mairabassetto@gmail.com>
2022-02-15 19:40:17 -0300

    Data prefetch

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,4 @@
-import Link from "next/link";
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, Spinner, Link } from "@chakra-ui/react";
 import { RiAddLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
@@ -7,6 +6,8 @@ import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 import { useUsers } from "../../services/hooks/useUsers";
 import { useState } from "react";
+import { queryClient } from "../../services/queryClient";
+import { api } from "../../services/api";
 
 export default function UserList() {
   const [currentPage, setCurrentPage] = useState(1);
@@ -16,6 +17,16 @@ export default function UserList() {
     base: false,
     lg: true,
   })
+  
+  async function handlePrefetchUser(userId: number) {
+    await queryClient.prefetchQuery(['user', userId], async () => {
+      const response = await api.get(`users/${userId}`)
+
+      return response.data;
+    }, {
+      staleTime: 1000 * 60 * 10, //10 minutos
+    })
+  }
 
   return(
     <Box>
@@ -76,7 +87,9 @@ export default function UserList() {
                         </Td>
                         <Td>
                           <Box>
-                            <Text fontWeight="bold"> {user.name} </Text>
+                            <Link color="purple.400" onMouseEnter={() => handlePrefetchUser(user.id)}>
+                              <Text fontWeight="bold"> {user.name} </Text>
+                            </Link>
                             <Text fontSize="sm" color="gray.300"> {user.email}</Text>
                           </Box>
                         </Td>
